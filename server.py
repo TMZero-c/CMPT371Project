8c80b3b3a27b38c51f1e50d32aaba48fa761fff4
Willywum <will.ch2705@gmail.com>
2025-04-05 14:38:54 -0700

    gameloop started

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import random
 
 clients = {}
 
@@ -31,6 +32,20 @@ def handle_client(client, addr):
     del clients[client]
     broadcast(f"[SERVER] {display_name} has left the chat.\n", None)
 
+def GameLoopinit(clients):
+    topicList = ["food", "cars", "anime", "movies", "school", "trains", "shervin", "IEEE", "the state of vancouver's economy in chinese", "clothing", "canada", "computer parts", "games", "art"]
+
+    print("Game Has Started!!")
+    # role and topic given to players
+    innocentTopic = random.choice(topicList)
+    innocent = "you're in the majority, you must figure out who the impostor is. The Topic is: " + innocentTopic
+    impostor = "you're the impostor, don't get found out, the other innocents have a topic, you need to blend in"
+    broadcast_except_random(innocent, impostor)
+    
+    
+
+
+
 def broadcast(message, sender):
     for client in clients:
         if client != sender:
@@ -40,6 +55,27 @@ def broadcast(message, sender):
                 client.close()
                 del clients[client]
 
+def broadcast_except_random(message, impostMessage): # Used in game logic to determine impostor 
+    if not clients:
+        return  # no clients connected
+
+    excluded_client = random.choice(list(clients))
+    
+    for client in clients:
+        if client != excluded_client:
+            try:
+                client.send(message.encode())
+            except:
+                client.close()
+                del clients[client]
+        elif client == excluded_client:
+            try:
+                client.send(impostMessage.encode())
+            except:
+                client.close()
+                del clients[client]
+
+
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("0.0.0.0", 5555))
@@ -48,6 +84,7 @@ def start_server():
 
     while True:
         client, addr = server.accept()
+        # Each thread contains a client during server runtime
         threading.Thread(target=handle_client, args=(client, addr)).start()
 
 if __name__ == "__main__":
